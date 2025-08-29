022bcc9b57cff24f0e6e9caddea3447c71c1b03b
chris <christopher.cortsen@gmail.com>
2025-08-30 00:14:27 +0200

    bug fixes

--- csv_reader.py
+++ csv_reader.py
@@ -23,11 +23,9 @@ with open('testcsv.csv', mode='r') as csvfile:
         #splitting up types
         types = card_info[2].split("-")
         mType = types[0].strip()
-        sTypes = []
-        for styp in types[-1:]:
-            sTypes.append(styp.strip())
-    
-
+        
+        sTypes = types[-1:][0].strip().split(" ")
+        
         #Assign data to object
         card0 = Card(card_info[0],card_info[1],mType,sTypes,card_info[3],card_info[4])
     
@@ -46,8 +44,8 @@ with open('testcsv.csv', mode='r') as csvfile:
             with open(fileName, "x") as f:
                 f.write("---")
                 f.write("\n tags: ")
-                for stype in card0.subtypes:
-                    f.write("\n --" + stype)
+                for stype in sTypes:
+                    f.write("\n - " + stype)
                 f.write("\n Type: " + card0.type)
                 f.write("\n Color: " + card0.color)
                 f.write("\n CMC: " + card0.cmc)
